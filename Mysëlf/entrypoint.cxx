871c086835e69eb538f89f57531738de3e9515d4
Kaii Misa <168567851+kaiiskirrrrr@users.noreply.github.com>
2024-11-19 10:27:28 +1100

    Update entrypoint.cxx

--- Mysëlf/entrypoint.cxx
+++ Mysëlf/entrypoint.cxx
@@ -12,36 +12,18 @@
 #pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "d3dx11.lib")
 
-struct entry
+auto throw_error(const char* error_message) -> void
 {
-	auto enable_virtual_terminal_processing() -> void
-	{
-		HANDLE console_handle = GetStdHandle(STD_OUTPUT_HANDLE);
-		DWORD console_mode = 0;
-
-		if (GetConsoleMode(console_handle, &console_mode))
-		{
-			console_mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
-			SetConsoleMode(console_handle, console_mode);
-		}
-	}
-
-	auto throw_error(const char* error_message) -> void
-	{
-		MessageBoxA(nullptr, error_message, "no bueno!", MB_ICONERROR | MB_OK);
-		ExitProcess(EXIT_FAILURE);
-	}
-};
+	MessageBoxA(nullptr, error_message, "no bueno!", MB_ICONERROR | MB_OK);
+	ExitProcess(EXIT_FAILURE);
+}
 
 auto __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) -> std::int32_t
 {
-	entry entry_point;
-	entry_point.enable_virtual_terminal_processing();
-
-	if (!bpro::connection_t->connect_port()) { entry_point.throw_error("kmbox port not found"); }
-	if (!request->prepare_modules()) { entry_point.throw_error("failed to find modules"); }
-	if (!request->start_handle()) { entry_point.throw_error("failed to connect to dma handle"); }
-	if (!keyboard_manager_t->connect()) { entry_point.throw_error("failed to connect to keyboard manager"); }
+	if (!bpro::connection_t->connect_port()) { throw_error("kmbox port not found"); }
+	if (!request->prepare_modules()) { throw_error("failed to find modules"); }
+	if (!request->start_handle()) { throw_error("failed to connect to dma handle"); }
+	if (!keyboard_manager_t->connect()) { throw_error("failed to connect to keyboard manager"); }
 
 	std::jthread([] { recoil_t->start(); }).detach();
 
